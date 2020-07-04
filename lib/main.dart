a61b6df2891cfa61af786e978acb0b1a40bd939a
Andrevnp <andre.vnp@live.com>
2020-07-03 21:57:26 -0300

    Design e Campus

--- lib/main.dart
+++ lib/main.dart
@@ -26,20 +26,49 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(
-            Icons.person_outline, 
-            size: 120.0, 
-          color: Colors.green),
-          TextField(keyboardType: TextInputType.number,
-          decoration: InputDecoration(labelText: "Peso em Kg",
-          labelStyle: TextStyle(color: Colors.green)),
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.green),
-          )
-        ],
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(20.0, 130, 20.0, 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Peso em (Kg)",
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Altura (cm)",
+                  labelStyle: TextStyle(color: Colors.green)),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 25.0),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(fontSize: 25.0, color: Colors.white),
+                  ),
+                  color: Colors.green,
+                ),
+              ),
+            ),
+            Text(
+              "Info",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 25.0, color: Colors.green),
+            )
+          ],
+        ),
       ),
     );
   }
